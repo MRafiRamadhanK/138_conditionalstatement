da265bad6c08d89befa10cc2c2718fc4a1d9dc97
M.RAFI RAMADHAN K <M.RAFI RAMADHAN K@VICTUSRAFI27100>
2024-03-04 11:55:34 +0700

    Add project files.

---
+++ samole prak 2/samole prak 2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+	srand(time(0));
+
+	int nBilA, nBilB;
+	nBilA = rand() % 11;
+	nBilB = rand() % 11;
+
+	string status;
+
+	if (nBilA == nBilB) {
+		status = "bilangannya sama";
+	}
+	else if (nBilA > nBilB) {
+		status = "Bilangan A lebih besar dari Bilangan B";
+	}
+	else
+	{
+		status = "Bilangan A lebih kecil dari Bilangan B";
+	}
+
+	cout << "Bilangan A =" << nBilA << endl;
+	cout << "Bilangan B =" << nBilB << endl;
+	cout << "statusnya =" << status << endl;
+}
